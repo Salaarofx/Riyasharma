6f4e644db71051b187c564ce365f858f700e3e0c
Salaarofx <139074058+Salaarofx@users.noreply.github.com>
2024-01-17 15:01:55 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,12 +26,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["✨️𝗢𝘄𝗻𝗲𝗿✨️"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["✨️𝗦𝘂𝗽𝗽𝗼𝗿𝘁✨️"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["𝗨𝗽𝗱𝗮𝘁𝗲"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["✨️𝗢𝘄𝗻𝗲𝗿✨️"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
